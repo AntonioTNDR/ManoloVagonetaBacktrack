b99d7ea8d0ecda3ed13dd8af0ba9601221116e20
ByDageta <119523917+ByDageta@users.noreply.github.com>
2024-05-06 10:50:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,50 +1,51 @@
-#include <stdio.h>
+/*
+    DYNAMICAL PROGRAMMING ASSIGNMENT
+    - David García Guirado - 55052860Z
+    - Jorge García Marín - 49314477Q
+    - Antonio Tendero Beltrán - 49428050S
+*/
 
 #include <stdio.h>
 
 void backtrack(int weights[], int sol[], int totalW[],  int nElements);
-const int cap = 200;
+const int CAP = 20;
+
 int main() {
-
-    int sol[20];
+    int sol[100];
     int totalW[4];
-    int nElements = 1;
+    int nElements = 0;
     int value;
-    int count = 0;
-    int weights[20];
+    int weights[100];
 
+    // Initialize the arrays
     for (int i = 0; i < 4; i++) {
         totalW[i] = 0;
     }
     for (int i = 0; i < 20; i++) {
-        weights[i] = 0;
-    }
-
-    for (int i = 0; i < 20; i++) {
         sol[i] = 0;
     }
 
+    printf("Enter weight for each item:\n");
     while(1){
         scanf("%d", &value);
         if(value == -1)
             break;
-        weights[count] = value;
+        weights[nElements] = value;
         nElements++;
-        count++;
     }
 
-    backtrack(weights, sol, totalW, count);
-
+    backtrack(weights, sol, totalW, nElements);
 }
 
-void backtrack(int weights[20], int sol[6], int totalW[4], int nElements){
-    int reached = 1;
+void backtrack(int weights[], int sol[], int totalW[], int nElements){
+    int reached = 1, nLaps = 0;
     for (int i = 0; i < nElements;) {
+        nLaps++;
         sol[i]++;
         if (sol[i] == 4) {
             if (i == 0) {
                 printf("---A solution cannot be reached---");
-                i = 7;
+                i = nElements;
                 reached = 0;
                 continue;
             }
@@ -55,19 +56,22 @@ void backtrack(int weights[20], int sol[6], int totalW[4], int nElements){
                 continue;
             }
         }
-        if (totalW[sol[i]] + weights[i] > cap) { // dead node condition
+        if (totalW[sol[i]] + weights[i] > CAP) { // dead node condition
             continue;
         }
         totalW[sol[i]] += weights[i];
-        if (i == 5) { printf("---A solution can be reached---\n"); }
+        if (i == nElements - 1) { printf("---A solution can be reached---\n"); }
         i++;
     }
     if (reached) { //solution node
+        printf("\n");
         for (int i = 0; i <nElements; ++i) {
             printf("Item:%d(weight %d)--Lorry:%d\n", i + 1, weights[i], sol[i]);
         }
+        printf("\n");
         for (int i = 1; i < 4; ++i) {
             printf("Lorry %d->%d kilos\n",i,totalW[i]);
         }
     }
+    printf("\nNumber of laps: %d", nLaps);
 }
